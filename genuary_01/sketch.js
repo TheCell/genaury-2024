6b4a4c35e3d77f01eddfe082f5c515228c1f9625
TheCell <3481579+TheCell@users.noreply.github.com>
2024-01-02 12:57:03 +0100

    updated sketch

--- genuary_01/sketch.js
+++ genuary_01/sketch.js
@@ -11,26 +11,28 @@ const startupParameters = {
     background(options.background);
     angleMode(DEGREES);
     brush.scaleBrushes(1.5);
-    brush.field("waves");
+    brush.field("waves"); //curved, truncated, zigzag, seabed, waves
+    // brush.noField();
   }
 }
 
 const options = {
-  background: '#212121',
-  foreground: '#ffae23',
-  color1: "#2c695a",
-  color2: "#4ad6af",
-  color3: "#7facc6",
-  color4: "#4e93cc",
-  color5: "#f6684f",
-  color6: "#ffd300",
+  background: '#464343',
+  foreground: '#464343',
+  color1: "#FF8360",
+  color2: "#E8E288",
+  color3: "#7DCE82",
+  color4: "#3CDBD3",
+  color5: "#00FFF5",
   restart: function () {
     seed = Math.random() * 100000;
     randomSeed(seed);
     background(options.background);
+    subloop = false;
+    loop();
   },
   save: function () {
-    saveCanvas(`${new Date().getFullYear()}_seed-${seed}_date-${Date.now()}`, 'png');
+    saveCanvas(`${new Date().getFullYear()}_Genuary01_seed-${seed}_date-${Date.now()}`, 'png');
   },
   loadImage: function() {
     document.getElementById('fileselector').click();
@@ -38,13 +40,13 @@ const options = {
 }
 
 // Creating a GUI with options.
-var gui = new dat.GUI({name: 'Customization'});
-var startupParameterFolder = gui.addFolder('canvas options');
+let gui = new dat.GUI({name: 'Customization'});
+let startupParameterFolder = gui.addFolder('canvas options');
 gui.remember(startupParameters);
 startupParameterFolder.add(startupParameters, 'xSize', 200);
 startupParameterFolder.add(startupParameters, 'ySize', 200);
 startupParameterFolder.add(startupParameters, 'resizeCanvas');
-var folder1 = gui.addFolder('Setup options');
+let folder1 = gui.addFolder('Setup options');
 gui.remember(options);
 folder1.addColor(options, 'background');
 folder1.addColor(options, 'foreground');
@@ -53,7 +55,6 @@ folder1.addColor(options, 'color2');
 folder1.addColor(options, 'color3');
 folder1.addColor(options, 'color4');
 folder1.addColor(options, 'color5');
-folder1.addColor(options, 'color6');
 folder1.open();
 gui.add(options, 'restart');
 gui.add(options, 'save');
@@ -63,21 +64,31 @@ function setup() {
   options.restart();
 }
 
+let subloop = false;
+let currentY = 0;
 function draw() {
-  let palette = [options.color1, options.color2, options.color3, options.color4, options.color5, options.color6];
+  if (!subloop) {
+    background(options.background);
+    subloop = true;
+    currentY = 0;
+  }
+  let palette = [options.color1, options.color2, options.color3, options.color4, options.color5];
   translate(-width/2,-height/2);
-  
-  // brush.box() returns an array with available brushes
-  let available_brushes = brush.box();
-  console.log(available_brushes);
-  
-  // Set the stroke to a random brush, color, and weight = 1
-  // You set a brush like this: brush.set(name_brush, color, weight)  
-  brush.set(random(available_brushes), random(palette), 1);
-  // brush.set("hatch_brush", random(palette), 1);
+  brush.noStroke();
+  brush.noHatch();
+  let randY = random(40, 60);
+  randY = random(30, 70);
+  let randX = random(20, 50);
+  for (let x = 0; x < startupParameters.ySize; x += randX) {
+    brush.fill(random(palette), random(60, 100));
+    brush.rect(x, currentY, randY, randX, CENTER);
+    // fill(random(palette));
+    // rect(x, y, randY, randX);
+    randX = random(20, 50);
+  }
+  currentY += randY;
 
-  // Draw a random flowLine (x, y, length, direction)
-  brush.flowLine(random(width), random(height), random(300,800), random(0,360))
-
-  // image(gfx, 0, 0);
+  if (currentY >= startupParameters.xSize + randY) {
+    noLoop();
+  }
 }
